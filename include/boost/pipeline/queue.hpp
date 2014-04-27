abfeb325e5e7cb5878320ecffea22fd55d5c21b1
erenon <erenon2@gmail.com>
2014-04-27 11:44:58 +0200

    queue.hpp: dummy alias of std::deque

---
+++ include/boost/pipeline/queue.hpp
@@ -0,0 +1,27 @@
+/**
+ * Boost.Pipeline
+ *
+ * Copyright 2014 Benedek Thaler
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * See $PIPELINE_WEBSITE$ for documentation
+ */
+
+#ifndef BOOST_PIPELINE_QUEUE_HPP
+#define BOOST_PIPELINE_QUEUE_HPP
+
+#include <deque>
+
+namespace boost {
+namespace pipeline {
+
+template <typename T>
+using queue = std::deque<T>;
+
+} // namespace pipeline
+} // namespace boost
+
+#endif // BOOST_PIPELINE_QUEUE_HPP
