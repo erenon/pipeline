1adadebeb48819e66b4ff11cf631beb0f5f56c24
erenon <erenon2@gmail.com>
2014-05-01 00:47:37 +0200

    segment_capture: determine segment type using pure TMP

--- test/segment_capture.cpp
+++ test/segment_capture.cpp
@@ -15,157 +15,252 @@
 #include <typeindex>
 #include <functional>
 
+#include <cxxabi.h>
+
 #include <boost/pipeline.hpp>
+#include <boost/preprocessor/stringize.hpp>
 
 #define BOOST_TEST_MODULE SegmentCapture
 #include <boost/test/unit_test.hpp>
 
-class capture
+using namespace boost::pipeline;
+
+struct invalid_transformation;
+
+template <typename Plan, typename Callable>
+class member_connector;
+
+template <typename Plan, typename Bind>
+class bind_connector;
+
+template <typename Plan, typename Trafo>
+class connector
 {
-  std::string getTypeName(const std::type_index& idx)
-  {
-    if (idx == std::type_index(typeid(std::size_t)))
-    {
-      return "std::size_t";
-    }
+  typedef typename Plan::value_type input;
 
-    if (idx == std::type_index(typeid(std::string)))
-    {
-      return "std::string";
-    }
+  template <typename Q>
+  using if_queue_back = typename std::enable_if<
+    std::is_same<typename std::decay<Q>::type, queue_back<input>>::value
+  ,int>::type;
 
-    if (idx == std::type_index(typeid(void)))
-    {
-      return "void";
-    }
+  template <typename Q>
+  using not_queue_back = typename std::enable_if<
+    ! std::is_same<typename std::decay<Q>::type, queue_back<input>>::value
+  ,int>::type;
 
-    if (idx == std::type_index(typeid(boost::pipeline::queue<std::size_t>)))
-    {
-      return "boost::pipeline::queue<std::size_t>";
-    }
+  //
+  // Connect function pointers
+  //
 
-    if (idx == std::type_index(typeid(boost::pipeline::queue_front<std::size_t>)))
-    {
-      return "boost::pipeline::queue_front<std::size_t>";
-    }
+  // Function pointer, single non-queue arg -> 1-1 Trafo
+  template <typename R, typename A, not_queue_back<A> = 0>
+  static one_one_segment<Plan, R> connect(R(function)(A));
 
-    if (idx == std::type_index(typeid(boost::pipeline::queue_back<std::size_t>)))
-    {
-      return "boost::pipeline::queue_back<std::size_t>";
-    }
+  // Function pointer, single queue arg -> N-1 Trafo (Aggregation)
+  template <typename R, typename A, if_queue_back<A> = 0>
+  static n_one_segment<Plan, R> connect(R(function)(A));
 
-    if (idx == std::type_index(typeid(boost::pipeline::queue<std::string>)))
-    {
-      return "boost::pipeline::queue<std::string>";
-    }
+  // Function pointer, two args, first is non queue -> 1-N Trafo
+  template <typename R, typename A, typename B, not_queue_back<A> = 0>
+  static one_n_segment<Plan, typename std::remove_reference<B>::type::value_type, R> connect(R(function)(A, B));
 
-    if (idx == std::type_index(typeid(boost::pipeline::queue_front<std::string>)))
-    {
-      return "boost::pipeline::queue_front<std::string>";
-    }
+  // Function pointer, two args, first queue -> N-M Trafo
+  template <typename R, typename A, typename B, if_queue_back<A> = 0>
+  static n_m_segment<Plan, typename std::remove_reference<B>::type::value_type, R> connect(R(function)(A, B));
 
-    if (idx == std::type_index(typeid(boost::pipeline::queue_back<std::string>)))
-    {
-      return "boost::pipeline::queue_back<std::string>";
-    }
+  //
+  // Connect std::function
+  //
 
-    return idx.name();
-  }
-protected:
-  template <typename R, typename A, typename B>
-  void printCapture()
-  {
-    std::cout
-      << "Result: " << getTypeName(typeid(R)) << std::endl
-      << "Arg 1 : " << getTypeName(typeid(A)) << std::endl
-      << "Arg 2 : " << getTypeName(typeid(B)) << std::endl
-    ;
-  }
+//  // std::function, single non-queue arg -> 1-1 Trafo
+//  template <typename R, typename A, not_queue_back<A> = 0>
+//  static one_one_segment<Plan, R> connect(const std::function<R(A)>& function);
+//
+//  // std::function, single queue arg -> N-1 Trafo (Aggregation)
+//  template <typename R, typename A, if_queue_back<A> = 0>
+//  static n_one_segment<Plan, R> connect(const std::function<R(A)>& function);
+//
+//  // std::function, two args, first is non queue -> 1-N Trafo
+//  template <typename R, typename A, typename B, not_queue_back<A> = 0>
+//  static one_n_segment<Plan, typename std::remove_reference<B>::type::value_type, R>
+//  connect(const std::function<R(A, B)>& function);
+//
+//  // std::function, two args, first queue -> N-M Trafo
+//  template <typename R, typename A, typename B, if_queue_back<A> = 0>
+//  static n_m_segment<Plan, typename std::remove_reference<B>::type::value_type, R>
+//  connect(const std::function<R(A, B)>& function);
+
+
+  // Connect functor type (includes lambda expressions and std::function)
+  // which has operator()
+  template <typename Callable, typename std::enable_if<
+         std::is_class<Callable>::value
+    && ! std::is_bind_expression<Callable>::value
+  ,int>::type = 0>
+  static auto connect(const Callable& lambda)
+    -> typename member_connector<Plan, Callable>::type
+  ;
+
+  // Connect std::bind expression
+  template <typename Bind, typename std::enable_if<
+    std::is_bind_expression<Bind>::value
+  ,int>::type = 0>
+  static auto connect(const Bind& bind)
+    -> typename bind_connector<Plan, Bind>::type
+  ;
+
+//  static invalid_transformation connect(...);
+public:
+  typedef decltype(connect(std::declval<Trafo>())) type;
 };
 
-class function_capture : private capture
+template <typename Plan, typename Callable>
+class member_connector
 {
-  // operator() const, single arg
-  template <typename Callable, typename R, typename A>
-  void captureMember(R(Callable::*function)(A) const)
-  {
-    (void) function;
+  typedef typename Plan::value_type input;
 
-    printCapture<R, A, void>();
-  }
+  template <typename Q>
+  using if_queue_back = typename std::enable_if<
+    std::is_same<typename std::decay<Q>::type, queue_back<input>>::value
+  ,int>::type;
 
-  // operator() const, two args
-  template <typename Callable, typename R, typename A, typename B>
-  void captureMember(R(Callable::*function)(A, B) const)
-  {
-    (void) function;
+  template <typename Q>
+  using not_queue_back = typename std::enable_if<
+    ! std::is_same<typename std::decay<Q>::type, queue_back<input>>::value
+  ,int>::type;
 
-    printCapture<R, A, B>();
-  }
+  // Note: mutable labdas have non-const operator()
 
-  // operator() non-const, single arg
-  template <typename Callable, typename R, typename A>
-  void captureMember(R(Callable::*function)(A))
-  {
-    (void) function;
+  // operator() const, single non-queue arg -> 1-1 Trafo
+  template <typename R, typename A, not_queue_back<A> = 0>
+  static one_one_segment<Plan, R> connect(R(Callable::*function)(A) const);
 
-    printCapture<R, A, void>();
-  }
+  // operator() non-const, single non-queue arg -> 1-1 Trafo
+  template <typename R, typename A, not_queue_back<A> = 0>
+  static one_one_segment<Plan, R> connect(R(Callable::*function)(A));
 
-  // operator() non-const, two args
-  template <typename Callable, typename R, typename A, typename B>
-  void captureMember(R(Callable::*function)(A))
-  {
-    (void) function;
+  // operator() const, single queue arg -> N-1 Trafo (Aggregation)
+  template <typename R, typename A, if_queue_back<A> = 0>
+  static n_one_segment<Plan, R> connect(R(Callable::*function)(A) const);
 
-    printCapture<R, A, B>();
-  }
+  // operator() non-const, single queue arg -> N-1 Trafo (Aggregation)
+  template <typename R, typename A, if_queue_back<A> = 0>
+  static n_one_segment<Plan, R> connect(R(Callable::*function)(A));
+
+  // operator() const, two args first is non queue -> 1-N Trafo
+  template <typename R, typename A, typename B, not_queue_back<A> = 0>
+  static one_n_segment<Plan, typename std::remove_reference<B>::type::value_type, R>
+  connect(R(Callable::*function)(A, B) const);
+
+  // operator() const, two args first queue -> N-M Trafo
+  template <typename R, typename A, typename B, if_queue_back<A> = 0>
+  static n_m_segment<Plan, typename std::remove_reference<B>::type::value_type, R>
+  connect(R(Callable::*function)(A, B) const);
+
+  // operator() non-const, two args first is non queue -> 1-N Trafo
+  template <typename R, typename A, typename B, not_queue_back<A> = 0>
+  static one_n_segment<Plan, typename std::remove_reference<B>::type::value_type, R>
+  connect(R(Callable::*function)(A));
+
+  // operator() non-const, two args first queue -> N-M Trafo
+  template <typename R, typename A, typename B, if_queue_back<A> = 0>
+  static n_m_segment<Plan, typename std::remove_reference<B>::type::value_type, R>
+  connect(R(Callable::*function)(A));
 
 public:
-  // Function pointer, single arg
-  template <typename R, typename A>
-  void capture(R(function)(A))
+  typedef decltype(connect(&Callable::operator())) type;
+};
+
+template <typename Plan, typename Bind>
+class bind_connector
+{
+  typedef typename Plan::value_type input;
+  typedef typename Bind::result_type R;
+
+  template <typename Bind1, typename R1>
+  class is_one_one_trafo
   {
-    (void) function;
+    template <
+      typename Bind2,
+      typename R2,
+      typename std::enable_if<std::is_same<
+        R2,
+        decltype(std::declval<Bind2>()(
+          std::declval<input>()
+        ))
+      >::value, int>::type = 0
+    >
+    static int test(int);
 
-    printCapture<R, A, void>();
-  }
+    template <typename Bind2, typename R2>
+    static long test(...);
 
-  // Functin pointer, two args
-  template <typename R, typename A, typename B>
-  void capture(R(function)(A, B))
+  public:
+    enum { value = std::is_same<int, decltype(test<Bind1, R1>(0))>::value };
+  };
+
+  template <typename Bind1, typename R1>
+  class is_n_one_trafo
   {
-    (void) function;
+    template <
+      typename Bind2,
+      typename R2,
+      typename std::enable_if<std::is_same<
+        R2,
+        decltype(std::declval<Bind2>()(
+          std::declval<queue_back<input>&>()
+        ))
+      >::value, int>::type = 0
+    >
+    static int test(int);
 
-    printCapture<R, A, B>();
-  }
+    template <typename Bind2, typename R2>
+    static long test(...);
 
-  // std::function, single arg
-  template <typename R, typename A>
-  void capture(const std::function<R(A)>& function)
-  {
-    (void) function;
+  public:
+    enum { value = std::is_same<int, decltype(test<Bind1, R1>(0))>::value };
+  };
 
-    printCapture<R, A, void>();
-  }
+  // operator() template, 1-1
+  template <typename Bind1, typename std::enable_if<
+    is_one_one_trafo<Bind1, R>::value
+  ,int>::type = 0>
+  static one_one_segment<Plan, R> connectCallOpTemplate();
 
-  // std::function, two args
-  template <typename R, typename A, typename B>
-  void capture(const std::function<R(A, B)>& function)
-  {
-    (void) function;
+  // operator() template, 1-N
+  template <typename Bind1, typename std::enable_if<
+    std::is_same<
+      R,
+      decltype(std::declval<Bind1>()(
+        std::declval<input>(),
+        std::declval<boost::pipeline::queue_front<R>&>()
+      ))
+    >::value
+  && ! is_one_one_trafo<Bind1, R>::value
+  ,int>::type = 0>
+  static one_n_segment<Plan, R, R> connectCallOpTemplate();
 
-    printCapture<R, A, B>();
-  }
+  // operator() template, N-1
+  template <typename Bind1, typename std::enable_if<
+    is_n_one_trafo<Bind1, R>::value
+  ,int>::type = 0>
+  static n_one_segment<Plan, R> connectCallOpTemplate();
 
-  // has operator()
-  template <typename Callable>
-  void capture(const Callable& lambda)
-  {
-    (void) lambda;
+  // operator() template, N-M
+  template <typename Bind1, typename std::enable_if<
+    std::is_same<
+      R,
+      decltype(std::declval<Bind1>()(
+        std::declval<boost::pipeline::queue_back<input>&>(),
+        std::declval<boost::pipeline::queue_front<R>&>()
+      ))
+    >::value
+  && ! is_n_one_trafo<Bind1, R>::value
+  ,int>::type = 0>
+  static n_m_segment<Plan, R, R> connectCallOpTemplate();
 
-    captureMember(&Callable::operator());
-  }
+public:
+  typedef decltype(connectCallOpTemplate<Bind>()) type;
 };
 
 std::size_t len1(const std::string& str)
@@ -173,7 +268,7 @@ std::size_t len1(const std::string& str)
   return str.size();
 }
 
-void len2(const std::string& str, boost::pipeline::queue_front<std::size_t>& out)
+void len2(const std::string& str, queue_front<std::size_t>& out)
 {
   auto len = str.size();
   if (len >= 3)
@@ -183,8 +278,8 @@ void len2(const std::string& str, boost::pipeline::queue_front<std::size_t>& out
 }
 
 void len3(
-  const boost::pipeline::queue_back<std::string>& in,
-  boost::pipeline::queue_front<std::size_t>& out
+  const queue_back<std::string>& in,
+  queue_front<std::size_t>& out
 )
 {
   for (const auto& str : in)
@@ -193,54 +288,85 @@ void len3(
   }
 }
 
+struct dummy_plan
+{
+  typedef std::string value_type;
+};
+
+class capture
+{
+  static std::string getTypeName(const std::type_index& idx)
+  {
+    int status = 0;
+    auto name = abi::__cxa_demangle(idx.name(), nullptr, nullptr, &status);
+
+    if (name)
+    {
+      std::string str(name);
+      free(name);
+      return str;
+    }
+    else
+    {
+      std::cerr << "Failed to demangle, status: " << status << std::endl;
+      return "N/A";
+    }
+  }
+
+public:
+  template <typename Trafo>
+  static void printCapture(const char* expr)
+  {
+    std::cout << expr << " : " << getTypeName(
+      typeid(typename connector<dummy_plan, Trafo>::type)
+    ) << std::endl;
+  }
+};
+
+#define CAPTURE(expr) capture::printCapture<decltype(expr)>(BOOST_PP_STRINGIZE(expr))
+
 BOOST_AUTO_TEST_CASE(SegmentCaptureFunctionPtr)
 {
   std::cout << "\n\nCapture function pointers:\n";
 
-  function_capture smt;
-
-  smt.capture(len1);
-  smt.capture(len2);
-  smt.capture(len3);
+  CAPTURE(len1);
+  CAPTURE(len2);
+  CAPTURE(len3);
 }
 
 BOOST_AUTO_TEST_CASE(SegmentCaptureStdFunction)
 {
   std::cout << "\n\nCapture std::functions:\n";
 
-  function_capture smt;
+  auto oo = std::function<std::size_t(const std::string&)>(len1);
+  CAPTURE(oo);
 
-  auto f1 = std::function<std::size_t(const std::string&)>(len1);
-  smt.capture(f1);
-
-  auto f2 = std::function<
+  auto on = std::function<
     void(const std::string&, boost::pipeline::queue_front<std::size_t>&)
   >(len2);
-  smt.capture(f2);
+  CAPTURE(on);
 
-  auto f3 = std::function<void(
+  auto nn = std::function<void(
     const boost::pipeline::queue_back<std::string>& in,
     boost::pipeline::queue_front<std::size_t>& out
   )>(len3);
-  smt.capture(f3);
+  CAPTURE(nn);
 }
 
 BOOST_AUTO_TEST_CASE(SegmentCaptureLambda)
 {
   std::cout << "\n\nCapture lambda expressions:\n";
 
-  function_capture smt;
+  auto oo = [](const std::string& str) { return str.size(); };
+  auto oom = [](const std::string& str) mutable { return str.size(); };
 
-  smt.capture([](const std::string& str) { return str.size(); });
-  smt.capture([](const std::string& str) mutable { return str.size(); });
-
-  smt.capture([](const std::string& str, boost::pipeline::queue_front<std::size_t>& out)
+  auto on = [](const std::string& str, boost::pipeline::queue_front<std::size_t>& out)
     {
       out.push_back(str.size());
     }
-  );
+  ;
 
-  smt.capture(
+  auto nm =
     [](
       const boost::pipeline::queue_back<std::string>& in,
       boost::pipeline::queue_front<std::size_t>& out
@@ -251,7 +377,12 @@ BOOST_AUTO_TEST_CASE(SegmentCaptureLambda)
         out.push_back(str.size());
       }
     }
-  );
+  ;
+
+  CAPTURE(oo);
+  CAPTURE(oom);
+  CAPTURE(on);
+  CAPTURE(nm);
 }
 
 struct len_t
@@ -266,127 +397,9 @@ BOOST_AUTO_TEST_CASE(SegmentCaptureFunctor)
 {
   std::cout << "\n\nCapture functor:\n";
 
-  function_capture smt;
-
-  len_t functor;
-  smt.capture(functor);
+  len_t oo_functor;
+  CAPTURE(oo_functor);
 }
-
-template <typename Value>
-class bind_capture : private capture
-{
-  template <typename Bind, typename R>
-  class is_one_one_trafo
-  {
-    template <
-      typename Bind1,
-      typename R1,
-      typename std::enable_if<std::is_same<
-        R1,
-        decltype(std::declval<Bind1>()(
-          std::declval<Value>()
-        ))
-      >::value, int>::type = 0
-    >
-    static int test(int);
-
-    template <typename Bind1, typename R1>
-    static long test(...);
-
-  public:
-    enum { value = std::is_same<int, decltype(test<Bind, R>(0))>::value };
-  };
-
-  template <typename Bind, typename R>
-  class is_n_one_trafo
-  {
-    template <
-      typename Bind1,
-      typename R1,
-      typename std::enable_if<std::is_same<
-        R1,
-        decltype(std::declval<Bind1>()(
-          std::declval<boost::pipeline::queue_back<Value>&>()
-        ))
-      >::value, int>::type = 0
-    >
-    static int test(int);
-
-    template <typename Bind1, typename R1>
-    static long test(...);
-
-  public:
-    enum { value = std::is_same<int, decltype(test<Bind, R>(0))>::value };
-  };
-
-  // operator() template, 1-1
-  template <typename Bind, typename R>
-  typename std::enable_if<
-    is_one_one_trafo<Bind, R>::value
-  >::type
-  captureCallOpTemplate()
-  {
-    printCapture<R, Value, void>();
-  }
-
-  // operator() template, 1-N
-  template <typename Bind, typename R>
-  typename std::enable_if<
-    std::is_same<
-      R,
-      decltype(std::declval<Bind>()(
-        std::declval<Value>(),
-        std::declval<boost::pipeline::queue_front<R>&>()
-      ))
-    >::value
-  && ! is_one_one_trafo<Bind, R>::value
-  >::type
-  captureCallOpTemplate()
-  {
-    printCapture<R, Value, boost::pipeline::queue_front<R>&>();
-  }
-
-  // operator() template, N-1
-  template <typename Bind, typename R>
-  typename std::enable_if<
-    is_n_one_trafo<Bind, R>::value
-  >::type
-  captureCallOpTemplate()
-  {
-    printCapture<R, boost::pipeline::queue_back<Value>&, void>();
-  }
-
-  // operator() template, N-M
-  template <typename Bind, typename R>
-  typename std::enable_if<
-    std::is_same<
-      R,
-      decltype(std::declval<Bind>()(
-        std::declval<boost::pipeline::queue_back<Value>&>(),
-        std::declval<boost::pipeline::queue_front<R>&>()
-      ))
-    >::value
-  && ! is_n_one_trafo<Bind, R>::value
-  >::type
-  captureCallOpTemplate()
-  {
-    printCapture<
-      R,
-      boost::pipeline::queue_front<Value>&,
-      boost::pipeline::queue_back<R>&
-    >();
-  }
-
-public:
-  template <typename Bind>
-  typename std::enable_if<std::is_bind_expression<Bind>::value>::type
-  capture(const Bind& binding)
-  {
-    (void) binding;
-
-    captureCallOpTemplate<Bind, typename Bind::result_type>();
-  }
-};
 
 std::size_t len_ntimes(std::size_t n, const std::string& str)
 {
@@ -448,10 +461,8 @@ BOOST_AUTO_TEST_CASE(SegmentCaptureStdBind)
 
   std::cout << "\n\nCapture std::bind expressions:\n";
 
-  bind_capture<std::string> smt;
-
-  smt.capture(std::bind(len_ntimes, 2, _1));
-  smt.capture(std::bind(len_min, 3, _1, _2));
-  smt.capture(std::bind(aggr_len_min, 3, _1));
-  smt.capture(std::bind(len_min_ntimes, 3, _1, _2));
+  CAPTURE(std::bind(len_ntimes, 2, _1));
+  CAPTURE(std::bind(len_min, 3, _1, _2));
+  CAPTURE(std::bind(aggr_len_min, 3, _1));
+  CAPTURE(std::bind(len_min_ntimes, 3, _1, _2));
 }
