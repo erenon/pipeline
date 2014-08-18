9cb09b099f5dc62a461ab31f773c5dabb6754b6f
erenon <erenon2@gmail.com>
2014-08-18 22:15:27 +0200

    segment<I,O> documentation added

--- include/boost/pipeline/type_erasure.hpp
+++ include/boost/pipeline/type_erasure.hpp
@@ -163,6 +163,13 @@ private:
 // segment<I,O> and specializations
 //
 
+/**
+ * Left-Right open series of transformations, the first consuming
+ * `Input` items and the last producing `Output` items.
+ *
+ * Connect this segment to `segment<terminated, Input>` and
+ * `segment<Output, terminated>` segments to create a runnable plan.
+ */
 template <typename Input, typename Output>
 class segment
 {
@@ -175,19 +182,38 @@ public:
   typedef Input  root_type;
   typedef Output value_type;
 
+  /** Copy constructor */
   segment(const segment<Input, Output>& rhs)
     :_impl(rhs._impl->clone())
   {}
 
+  /**
+   * Creates a segment from an open segment of unspecified type.
+   *
+   * @param impl Any kind of segment accepting `Input` and producing `Output`
+   *
+   * @code
+   * segment<std::string, std::size_t> s1 = make(length);
+   * @endcode
+   */
   segment(const detail::segment_concept<Input, Output>& impl)
     :_impl(impl.clone())
   {}
+
+  /** @cond internal */
 
   template <typename... Trafos>
   segment(const detail::open_segment<Trafos...>& impl)
     :_impl(impl.connect_to(proxy()).clone())
   {}
 
+  /** @endcond */
+
+  /**
+   * Produces a new segment by concatenating `rhs` after
+   * this segment. Please note, `this` and `rhs` remains
+   * unchanged.
+   */
   template <typename NewOutput>
   segment<Input, NewOutput>
   operator|(const segment<Output, NewOutput>& rhs) const
@@ -197,6 +223,13 @@ public:
     );
   }
 
+  /**
+   * Schedules this segment on `pool` and writes the produced output
+   * to `target`. This method is typically called by the library.
+   *
+   * @pre Segment must be connected to a parent segment,
+   * the uppermost parent must be left-terminated.
+   */
   void run(thread_pool& pool, const queue_back<Output>& target)
   {
     _impl->run(pool, target);
@@ -211,6 +244,13 @@ private:
   std::unique_ptr<detail::segment_concept<Input, Output>> _impl;
 };
 
+/**
+ * Left terminated series of transformations producing `Output`
+ * type of items at the end.
+ *
+ * Connect this segment to a `segment<Output, terminated>`
+ * segment to create a runnable plan.
+ */
 template <typename Output>
 class segment<terminated, Output>
 {
@@ -221,14 +261,29 @@ public:
   typedef terminated root_type;
   typedef Output     value_type;
 
+  /** Copy constructor */
   segment(const segment<terminated, Output>& rhs)
     :_impl(rhs._impl->clone())
   {}
 
+  /**
+   * Creates a segment from any kind of matching segment of unspecified type.
+   *
+   * @param impl Any kind of left-terminated segment producing `Output`
+   *
+   * @code
+   * segment<terminated, std::string> s1 = from(lines) | trim;
+   * @endcode
+   */
   segment(const detail::segment_concept<terminated, Output>& impl)
     :_impl(impl.clone())
   {}
 
+  /**
+   * Produces a new segment by concatenating `rhs` after
+   * this segment. Please note, `this` and `rhs` remains
+   * unchanged.
+   */
   template <typename NewOutput>
   segment<terminated, NewOutput>
   operator|(const segment<Output, NewOutput>& rhs) const
@@ -238,6 +293,10 @@ public:
     );
   }
 
+  /**
+   * Schedules this segment on `pool` and writes the produced output
+   * to `target`. This method is typically called by the library.
+   */
   void run(thread_pool& pool, const queue_back<Output>& target)
   {
     _impl->run(pool, target);
@@ -247,6 +306,13 @@ private:
   std::unique_ptr<detail::segment_concept<terminated, Output>> _impl;
 };
 
+/**
+ * Right terminated series of transformations consuming `Input`
+ * type of items.
+ *
+ * Connect this segment to a `segment<terminated, Input>`
+ * segment to create a runnable plan.
+ */
 template <typename Input>
 class segment<Input, terminated>
 {
@@ -256,13 +322,25 @@ class segment<Input, terminated>
   typedef detail::upstream_proxy<Input> proxy;
 
 public:
+  /** Copy constructor */
   segment(const segment<Input, terminated>& rhs)
     :_impl(rhs._impl->clone())
   {}
 
+  /**
+   * Creates a segment from any kind of matching segment of unspecified type.
+   *
+   * @param impl Any kind of right-terminated segment consuming `Input`
+   *
+   * @code
+   * segment<std::string, terminated> s1 = make(trim) | trimmed_lines;
+   * @endcode
+   */
   segment(const detail::segment_concept<Input, terminated>& impl)
     :_impl(impl.clone())
   {}
+
+  /** @cond internal */
 
   template <typename... Trafos>
   segment(const detail::open_segment<Trafos...>& impl)
@@ -274,7 +352,15 @@ public:
     :_impl((proxy() | impl).clone())
   {}
 
+  /** @endcond */
 
+  /**
+   * Schedules this segment on `pool`.
+   * This method is typically called by the library.
+   *
+   * @pre Segment must be connected to a parent segment,
+   * the uppermost parent must be left-terminated.
+   */
   execution run(thread_pool& pool)
   {
     return _impl->run(pool);
@@ -284,6 +370,9 @@ private:
   std::unique_ptr<detail::segment_concept<Input, terminated>> _impl;
 };
 
+/**
+ * A complete (left and right-terminated), runnable segment.
+ */
 template <>
 class segment<terminated, terminated>
 {
@@ -291,14 +380,30 @@ class segment<terminated, terminated>
   friend class segment;
 
 public:
+  /** Copy constructor */
   segment(const segment<terminated, terminated>& rhs)
     :_impl(rhs._impl->clone())
   {}
 
+  /**
+   * Creates a segment from any kind of matching segment of unspecified type.
+   *
+   * @param impl Any kind of complete segment
+   *
+   * @code
+   * segment<terminated, terminated> s = from(lines) | trim | trimmed_lines;
+   * s.run(pool);
+   * @endcode
+   */
   segment(const detail::segment_concept<terminated, terminated>& impl)
     :_impl(impl.clone())
   {}
 
+  /**
+   * Schedules the segment on `pool`.
+   *
+   * @returns An `execution` instance representing the running pipeline.
+   */
   execution run(thread_pool& pool)
   {
     return _impl->run(pool);
